e7d22b04d4c7b39c19b47032196e30320192bbff
Jerod Hammerstein <jh@jhtechservices.com>
2022-06-08 16:03:42 -0500

    remove NextRequest from _middleware

--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -1,12 +1,11 @@
 import { handleSitemapRequests } from '@faustjs/next';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export default async function _middleware(req) {
   const sitemapRequest = await handleSitemapRequests(req, {
     wpUrl: process.env.NEXT_PUBLIC_WORDPRESS_URL,
     sitemapIndexPath: `/sitemap_index.xml`,
   });
-
   if (sitemapRequest) {
     return sitemapRequest;
   }
